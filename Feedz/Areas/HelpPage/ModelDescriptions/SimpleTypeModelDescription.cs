86ad6ec3ca99e31e0880b038f51b5ae5fe5f504b
Nattapong Nunpan <aixasz@hotmail.com>
2016-12-21 14:36:21 +0700

    initial commit

---
+++ Feedz/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Feedz.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Feedz/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Feedz.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public async Task<IEnumerable<string>> Get()
+        {
+            return GetData().Result;
+        }
+
+        private Task<string[]> GetData()
+        {
+            return Task.Run(() => new[] { "value1", "value2" });
+        }
+    }
+}
